da9f622172793224ae9d6428edadcfd81404bff2
Vlad hoi <goyvlad2000@gmail.com>
2019-08-22 15:14:03 +0300

    Added custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@gmail.com'
+        password = '123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@gmaiL.com'
+        user = get_user_model().objects.create_user(email, '123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, '123')
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            '123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
